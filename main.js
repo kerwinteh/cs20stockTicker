b4d69151943e7728ae28abe58794fb8e6382f1d5
kerwinteh <kerwintehkerwei@gmail.com>
2023-04-24 17:45:08 -0400

    edit

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const {MongoClient} = require("mongodb");
+const uri = "mongodb+srv://kerwinteh:SYQ12nBg1QZKjs1F@cluster0.wfkcgkb.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+
 var http = require('http');
 var port = process.env.PORT || 3000;
 var fs = require('fs');
@@ -5,11 +9,16 @@ var fs = require('fs');
 // console.log("This goes to the console window");
 
 http.createServer(function (req, res) {
-    console.log("request made: " + req.url);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    var myReadStream = fs.createReadStream(__dirname + '/index.html', "utf8");
-    myReadStream.pipe(res);
-    // res.write("<h2>Hello World</h2>");
-    // res.write ("Success!  This app is deployed offline");
-    // res.end();
+    if(req.url === "/"){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write("THIS IS HOME PAGE");
+        var myReadStream = fs.createReadStream(__dirname + '/index.html', "utf8");
+        myReadStream.pipe(res);
+    } else if(req.url === "/process") {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write("THIS IS PROCESS PAGE");
+        var myReadStream = fs.createReadStream(__dirname + '/index.html', "utf8");
+        myReadStream.pipe(res);
+    }
+
 }).listen(port);
