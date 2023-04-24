423368564de345e1df19ec97019da9e6c65268c6
kerwinteh <kerwintehkerwei@gmail.com>
2023-04-24 19:54:48 -0400

    edit

--- main.js
+++ main.js
@@ -6,15 +6,16 @@ var http = require('http');
 var port = process.env.PORT || 3000;
 var fs = require('fs');
 
-
 http.createServer(function (req, res) {
+    console.log("heelloo");
     if(req.url === "/process") {
+        console.log("processpageeee");
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write("THIS IS PROCESS PAGE");
         var myReadStream = fs.createReadStream(__dirname + '/data.html', "utf8");
         myReadStream.pipe(res);
     } else {
-        console.log("request made");
+        console.log("HOME PAGEEE");
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write("HOME PAGE, PICK A STOCK");
         var myReadStream = fs.createReadStream(__dirname + '/index.html', "utf8");
