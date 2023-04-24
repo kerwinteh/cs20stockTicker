8fc75b2abc2ba5b9ac9b65501d3c6af86865b682
kerwinteh <kerwintehkerwei@gmail.com>
2023-04-24 19:15:48 -0400

    edit

--- main.js
+++ main.js
@@ -9,21 +9,21 @@ var fs = require('fs');
 // console.log("This goes to the console window");
 
 http.createServer(function (req, res) {
-    // if(req.url === "/"){
-    //     console.log("request made");
-    //     res.writeHead(200, {'Content-Type': 'text/html'});
-    //     res.write("THIS IS HOME PAGE");
-    //     var myReadStream = fs.createReadStream(__dirname + '/index.html', "utf8");
-    //     myReadStream.pipe(res);
-    // } else if(req.url === "/process") {
-    //     res.writeHead(200, {'Content-Type': 'text/html'});
-    //     res.write("THIS IS PROCESS PAGE");
-    //     var myReadStream = fs.createReadStream(__dirname + '/index.html', "utf8");
-    //     myReadStream.pipe(res);
-    // }
+    if(req.url === "/process") {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write("THIS IS PROCESS PAGE");
+        var myReadStream = fs.createReadStream(__dirname + '/data.html', "utf8");
+        myReadStream.pipe(res);
+    } else {
+        console.log("request made");
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write("HOME PAGE, PICK A STOCK");
+        var myReadStream = fs.createReadStream(__dirname + '/index.html', "utf8");
+        myReadStream.pipe(res);
+    }
 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("THIS IS HOME PAGE");
-    var myReadStream = fs.createReadStream(__dirname + '/index.html', "utf8");
-    myReadStream.pipe(res);
+    // res.writeHead(200, {'Content-Type': 'text/html'});
+    // res.write("THIS IS HOME PAGE");
+    // var myReadStream = fs.createReadStream(__dirname + '/index.html', "utf8");
+    // myReadStream.pipe(res);
 }).listen(port);
