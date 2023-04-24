add3e511e88e1a328646f109185ea1e9498bf7f0
kerwinteh <kerwintehkerwei@gmail.com>
2023-04-24 19:13:32 -0400

    edit

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const {MongoClient} = require("mongodb");
-const uri = "mongodb+srv://kerwinteh:SYQ12nBg1QZKjs1F@cluster0.wfkcgkb.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri);
+// const {MongoClient} = require("mongodb");
+// const uri = "mongodb+srv://kerwinteh:SYQ12nBg1QZKjs1F@cluster0.wfkcgkb.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri);
 
 var http = require('http');
 var port = process.env.PORT || 3000;
@@ -9,15 +9,21 @@ var fs = require('fs');
 // console.log("This goes to the console window");
 
 http.createServer(function (req, res) {
-    if(req.url === "/"){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write("THIS IS HOME PAGE");
-        var myReadStream = fs.createReadStream(__dirname + '/index.html', "utf8");
-        myReadStream.pipe(res);
-    } else if(req.url === "/process") {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write("THIS IS PROCESS PAGE");
-        var myReadStream = fs.createReadStream(__dirname + '/index.html', "utf8");
-        myReadStream.pipe(res);
-    }
+    // if(req.url === "/"){
+    //     console.log("request made");
+    //     res.writeHead(200, {'Content-Type': 'text/html'});
+    //     res.write("THIS IS HOME PAGE");
+    //     var myReadStream = fs.createReadStream(__dirname + '/index.html', "utf8");
+    //     myReadStream.pipe(res);
+    // } else if(req.url === "/process") {
+    //     res.writeHead(200, {'Content-Type': 'text/html'});
+    //     res.write("THIS IS PROCESS PAGE");
+    //     var myReadStream = fs.createReadStream(__dirname + '/index.html', "utf8");
+    //     myReadStream.pipe(res);
+    // }
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("THIS IS HOME PAGE");
+    var myReadStream = fs.createReadStream(__dirname + '/index.html', "utf8");
+    myReadStream.pipe(res);
 }).listen(port);
