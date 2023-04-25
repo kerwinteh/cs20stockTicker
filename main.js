9f8b3e19ad9f8f076ed84200614c6f9ee79f6f2a
kerwinteh <kerwintehkerwei@gmail.com>
2023-04-25 00:52:44 -0400

    edit

--- main.js
+++ main.js
@@ -93,12 +93,12 @@ async function searchDb(input, res, user) {
 
     if (user === "company") {
         console.log("IN COMPANIES");
-        let result = collection.find({name: input}).toArray();
+        let result = await collection.find({name: input}).toArray();
         res.write(JSON.stringify(result));
         console.log(result);
     } else {
         console.log("IN TICKERSYMBOLSSS");
-        let result = collection.find({ticker: input}).toArray();
+        let result = await collection.find({ticker: input}).toArray();
         res.write(JSON.stringify(result));
         console.log(result);
     }
