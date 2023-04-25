ff46f6ae73808fc5d6323ce965d874770fa3f14a
kerwinteh <kerwintehkerwei@gmail.com>
2023-04-25 01:02:14 -0400

    edit

--- main.js
+++ main.js
@@ -8,51 +8,6 @@ var qs = require('querystring');
 
 
 http.createServer(async function (req, res) {
-    // if (req.url === "/process") {
-    //     fs.readFile('data.html',function (err, data){
-    //         res.write("THIS IS PROCESS PAGE");
-    //         res.writeHead(200, {'Content-Type': 'text/html'});
-    //         res.write("data" + data);
-    //         res.end();
-    //     });
-    // } else {
-    //     fs.readFile('index.html',function (err, data){
-    //         res.write("index.html fillee");
-    //         res.writeHead(200, {'Content-Type': 'text/html'});
-    //         res.write("data" + data);
-    //         res.end();
-    //     });
-    // }
-
-    // fs.readFile('index.html',function (err, data){
-    //     res.write("index.html fillee");
-    //     res.writeHead(200, {'Content-Type': 'text/html'});
-    //     res.write("data" + data);
-    //     res.end();
-    // });
-
-    //
-    // if (req.url === "/process") {
-    //     await client.connect();
-    //     await client.db("stockTicker");
-    //     console.log("connected to server");
-    //
-    //     res.writeHead(200, {'Content-Type': 'text/html'});
-    //     res.write("THIS IS PROCESS PAGE");
-    //     var myReadStream = fs.createReadStream(__dirname + '/data.html', "utf8");
-    //     myReadStream.pipe(res);
-    //     myReadStream.on('data', function (chunk) {
-    //         console.log(chunk.toString());
-    //     });
-    // } else {
-    //     console.log("HOME PAGEEE");
-    //     res.writeHead(200, {'Content-Type': 'text/html'});
-    //     res.write("HOME PAGE, PICK A STOCK");
-    //     var myReadStream = fs.createReadStream(__dirname + '/index.html', "utf8");
-    //     myReadStream.pipe(res);
-    // }
-
-
     if (req.url === "/process") {
         //connect to mongo
         await connect();
@@ -62,18 +17,12 @@ http.createServer(async function (req, res) {
             //get user input
             let textinput = "";
             req.on('data', data => {
-                console.log(data.toString());
                 textinput += data.toString();
             });
-            console.log("textInput:" + textinput);
 
             req.on('end', async () => {
                 textinput = qs.parse(textinput);
-                console.log(textinput);
-                console.log("textinput['name']: " + textinput['name']);
-                console.log("textinput['user']: " + textinput['user']);
                 await searchDb(textinput['name'], res, textinput['user']);
-                console.log("ELORE");
             });
         });
     } else {
@@ -85,20 +34,17 @@ http.createServer(async function (req, res) {
     }
 }).listen(port);
 
+
 async function searchDb(input, res, user) {
     const database = client.db("stockTicker");
     const collection = database.collection("companies");
-    console.log("USER INPUT: " + input);
-    console.log("USER CHOICE: " + user);
     let result;
     if (user === "company") {
-        console.log("IN COMPANIES");
         result = await collection.find({name: input}).toArray();
         if (result.length === 0) {
             res.write("No results for: " + input);
         }
     } else {
-        console.log("IN TICKERSYMBOLSSS");
         result = await collection.find({ticker: input}).toArray();
         if (result.length === 0) {
             res.write("No results for: " + input);
@@ -115,72 +61,10 @@ async function searchDb(input, res, user) {
 
     await client.close();
     res.end();
-
-
-
-    // collection.findOne({}, function (err,result){
-    //     if (err) throw err;
-    //     res.write(result);
-    // });
-    // if(user) {
-    //     console.log("name" + name);
-    //     res.write(name);
-    //     // res.write(user);
-    //     // var query = { name: name };
-    //
-    //     res.write("beforeee");
-    //
-    //     collection.find(user ? {"name": name} : {"ticker": name + "\r"}).toArray(async function (err, result) {
-    //         if (err) throw err;
-    //         console.log(result);
-    //
-    //         if (result.length === 0) {
-    //             res.write("No results for: " + name)
-    //         }
-    //
-    //         res.write("resuilt" + result);
-    //         res.write("inside");
-    //         await client.close();
-    //     });
-    // }
-
-
-    // collection.findOne({}, function (err, result){
-    //     if (err) throw err;
-    //     res.write(result);
-    //     console.log(result);
-    //     res.write("inside");
-    // })
-
-    //     res.write("aftersecond");
-    // }
 }
+
 
 async function connect() {
-    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
-    // Establish and verify connection
     await client.db("stockTicker");
-    console.log("Connected successfully to server");
 }
-
-
-// const mongoose = require('mongoose');
-// function mongoose_connect()
-// {
-//     const url =""; // usual connection string goes here
-//     mongoose.connect(url,{ useUnifiedTopology: true,
-//         useNewUrlParser: true, useCreateIndex:true });
-//     const db = mongoose.connection;
-//     db.on('error', console.error.bind(console, 'error:'));
-//     db.once('open', function() {
-//         console.log("success!");
-//         Books.find(function (err, mybooks) {
-//             if (err) return console.error(err);
-//             for (i=0; i<mybooks.length; i++)
-//                 console.log(mybooks[i].show());
-//             db.close();
-//         }); // end find
-//     }); //end db once
-// } //end function
-// mongoose_connect();
